75f460a68d840b1cbba5dd74ed2cac5d100b5ad9
Aditya Fakhri <adityafakhri04@gmail.com>
2026-06-30 15:14:20 +0700

    feat(blade components): add footer copyright component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-[#2A1B60] text-white py-6 border-t border-white/10 mt-auto">
+    <div class="max-w-7xl mx-auto px-4 text-center text-sm font-medium tracking-wide">
+        Copyright &copy; 2025 SMAN 2 Bangkalan | Ahmad Jihaduddin Salim
+    </div>
+</footer>
